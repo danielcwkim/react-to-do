15c02b8204699e06f521679fe18c5dbe04da07ed
danielkim <danielkimcw@gmail.com>
2018-04-13 16:22:09 -0700

    assignment react events done

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
 import React, { Component } from 'react';
 import './App.css';
 import ToDo from './components/ToDo.js';
+import React, { Component } from 'react';
+import './App.css';
+import ToDo from './components/ToDo.js';
 
 class App extends Component {
-    constructor(props) {
-     super(props);
-     this.state = {
-       todos: [
-         { description: 'Walk the cat', isCompleted: true },
-         { description: 'Throw the dishes away', isCompleted: false },
-         { description: 'Buy new dishes', isCompleted: false }
+  constructor(props) {
+    super(props);
+    this.handleClick= this.handleClick.bind(this);
+    this.state = {
+      todos: [
+        { description: 'walk the cat', isCompleted: true},
+        { description: 'Throw the dishes away', isCompleted: false },
+        { description: 'Buy new dishes', isCompleted: false }
       ],
-      newTodoDescription: ''
-     };
-   }
+      newTodoDescription: '',
+    }
+  }
+  handleChange(e) {
+    this.setState({newTodoDescription: e.target.value})
+  }
 
-   handleChange(e) {
-     this.setState({ newTodoDescription: e.target.value })
-   }
-
-    handleSubmit(e) {
-      e.preventDefault();
-     if (!this.state.newTodoDescription) { return }
-     const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
-     this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
-   }
-
-  toggleComplete(index) {
+  handleSubmit(e){
+    e.preventDefault();
+    if (!this.state.newTodoDescription) { return }
+    const newTodo = { description: this.state.newTodoDescription, isCompleted: false};
+    this.setState({todos: [...this.state.todos, newTodo], newTodoDescription: '' });
+  }
+  toggleComplete(index){
     const todos = this.state.todos.slice();
     const todo = todos[index];
     todo.isCompleted = todo.isCompleted ? false : true;
-    this.setState({ todos: todos });
+    this.setState({todos: todos});
   }
- 
-   
+  deleteTodo(index){
+    const toDoList = this.state.todos.filter(x => x !== this.state.todos[index] );
+    this.setState({todos: toDoList});
+  }
+  handleClick(e){
+    this.deleteTodo(e)
+  }
   render() {
     return (
       <div className="App">
         <ul>
-          { this.state.todos.map( (todo, index) => 
-             <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } /> )}
-        </ul> 
-         <form onSubmit={ (e) => this.handleSubmit(e) }>
-           <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
-
-         </form> 
+          { this.state.todos.map((todo,index) =>
+            <ToDo key={index} description={ todo.description } isCompleted={todo.isCompleted} toggleComplete={() => this.toggleComplete(index)} deleteTodo={() => this.deleteTodo(index)} handleClick={() => this.handleClick(index)}/>
+          )}
+        </ul>
+        <form onSubmit={ (e) => this.handleSubmit(e)}>
+          <input type="text" value={ this.state.newTodoDescription} onChange={(e) => this.handleChange(e)}/>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 
- class ToDo extends Component {
-   render() {
-     return (
-       <li>
-         <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
-        <span>{this.props.description}</span> 
-       </li>
-     );
-   }
- }
- 
- export default ToDo;
+class ToDo extends Component {
+  render() {
+    return (
+      <li>
+        <input type="checkbox" checked={this.props.isCompleted} onChange={ this.props.toggleComplete}/>
+        <span>{this.props.description}</span>
+        <button onClick={this.props.handleClick}>Delete</button>
+      </li>
+    );
+  }
+}
+
+export default ToDo;
